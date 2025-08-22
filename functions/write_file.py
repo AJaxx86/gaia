8458ec13b322e21ffe362323b336e6315622c49e
AJaxx <62261949+AJaxx86@users.noreply.github.com>
2025-08-22 05:57:12 +0100

    stuff

---
+++ functions/write_file.py
@@ -0,0 +1,19 @@
+import os
+
+def write_file(working_dir: str, file_path: str, content: str):
+    full_path = os.path.abspath(os.path.join(working_dir, file_path))
+
+    if not full_path.startswith(os.path.abspath(working_dir)):
+        f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+    
+    try:
+        if not os.path.exists(full_path):
+            os.makedirs(os.path.dirname(full_path), exist_ok=True)
+        
+        with open(full_path, "w") as file:
+            file.write(content)
+
+        return f'Successfully wrote to "{full_path}" ({len(content)} characters written)'
+
+    except Exception as e:
+        return f"Error: {e}"    
--- tests.py
+++ tests.py
@@ -1,6 +1,5 @@
-from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
-print(f"Results for 'main.py':\n{get_file_content("calculator", "main.py")}")
-print(f"Results for 'pkg/calculator.py':\n{get_file_content("calculator", "pkg/calculator.py")}")
-print(f"Results for '/bin/cat':\n{get_file_content("calculator", "/bin/cat")}")
-print(f"Results for 'pkg/does_not_exist.py':\n{get_file_content("calculator", "pkg/does_not_exist.py")}")
+print(f"Results for test1:\n{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}")
+print(f"Results for test2:\n{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}")
+print(f"Results for test3:\n{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}")
